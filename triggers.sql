9aa338593074fa2fa60551e5935fdb31e0795243
ishvin712 <121254670+ishvin712@users.noreply.github.com>
2024-09-16 23:35:38 +0530

    Create triggers.sql
    PL/SQL Code to create Triggers

---
+++ triggers.sql
@@ -0,0 +1,27 @@
+-- Trigger to ensure unique registration number for TAXI table
+CREATE OR REPLACE TRIGGER unique_registration_no
+BEFORE INSERT OR UPDATE ON TAXI
+FOR EACH ROW
+DECLARE
+    reg_no_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO reg_no_count
+    FROM TAXI
+    WHERE Registration_no = :NEW.Registration_no;
+
+    IF reg_no_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Registration number must be unique.');
+    END IF;
+END;
+
+-- Trigger to decrease driver rating if feedback is bad
+CREATE OR REPLACE TRIGGER UPDATE_DRIVER_RATING
+AFTER INSERT ON FEEDBACK
+FOR EACH ROW
+WHEN (NEW.Message LIKE '%bad%')
+DECLARE
+    v_driver_id INT;
+BEGIN
+    SELECT driver_id INTO v_driver_id FROM TRIP_DETAILS WHERE trip_id = :NEW.Trip_id;
+    UPDATE DRIVER SET Rating = Rating - 1 WHERE driver_id = v_driver_id;
+END;
